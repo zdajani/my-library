10ede9e2de453287cdfa7eaa1b619c1e56ee80a7
zdajani <dajani.zeina@gmail.com>
2017-08-02 14:15:09 -0400

    add-redux-folder-structure

--- client/src/features/home/home.test.js
+++ client/src/features/home/home.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Welcome from './components/welcome';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Welcome />, div);
 });
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store } from './configureStore'
+
+import Welcome from './features/home/components/welcome'
+
+
+ReactDOM.render(
+  <Provider store={store}>
+  { /* ConnectedRouter will use the store from Provider automatically */ }
+    <Welcome />
+  </Provider>, 
+  document.getElementById('root')
+);
+
+
